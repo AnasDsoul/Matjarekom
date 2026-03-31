af8c0e2175528148152de0905f01c94f9ddfaad1
Anas Dsoul <anasdsoul08@gmail.com>
2026-03-31 19:11:58 +0100

    code fontion bien sans pagination, les probleme que j'ai rencontre : base Url automatisé fiat un problem aussi .text au lieux de json, et le truc le plus important que postman dans le code avec tous les languge souhaité surtout python

--- RecupCokiesID.py
+++ RecupCokiesID.py
@@ -1,46 +1,43 @@
 import requests
-import pandas as pd
-from urllib.parse import urljoin
-from pathlib import Path
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-SHOPNAME = os.getenv("SHOPNAME")
-PASSWORD = os.getenv("PASSWORD")
-BASE_URL = os.getenv("BASE_URL")
-
-# 🔹 Configuration
-LOGIN_URL = f"{BASE_URL}/login.jsp"
-
-# 🔹 1. LOGIN
 session = requests.Session()
 
-login_headers = {
-    "Accept": "application/json",
+# 1. LOGIN
+login_url = "https://v1.cathedis.delivery/callback"
+
+login_payload = {
+    "username": "Matjarekom",
+    "password": "C4z78uvH"
+}
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+login_response = session.post(login_url, data=login_payload, headers=headers)
+
+print("Login status:", login_response.status_code)
+print("Cookies:", session.cookies.get_dict())
+
+
+# 2. APPEL API
+url = "https://v1.cathedis.delivery/ws/action"
+
+payload = {
+    "action": "delivery.api.my",
+    "data": {
+        "context": {
+            "limit": 40,
+            "offset": 0
+        }
+    }
+}
+
+headers = {
     "Content-Type": "application/json"
 }
 
-login_payload = {
-    "username": SHOPNAME,
-    "password": PASSWORD
-}
-login_response = session.post(LOGIN_URL, json=login_payload, headers=login_headers)
+response = session.post(url, json=payload, headers=headers)
 
-# Vérification
-if login_response.status_code != 200:
-    print("❌ Erreur login :", login_response.status_code)
-    print(login_response.text)
-    exit()
-
-print("✅ Login réussi")
-
-# 🔹 2. Récupération des cookies
-cookies = session.cookies.get_dict()
-
-jsessionid = cookies.get("JSESSIONID")
-
-print("JSESSIONID:",jsessionid)
-
-
+print("Status:", response.status_code)
+print("Response:", response.text)
